4c006251f7eb8cd860982e42669d746ed9b9ca95
TEERAWAT PRAROM <teewat@yahoo.com>
2022-10-07 23:03:15 +1100

    git init

---
+++ src/db.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+  log: [
+    { level: 'warn', emit: 'event' },
+    { level: 'info', emit: 'event' },
+    { level: 'error', emit: 'event' },
+  ],
+})
+
+export default prisma
